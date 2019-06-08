588dbf94247f96d834faec927860563582d64257
贾少英 <jiashaoying@shgsec.com>
2019-06-08 12:19:00 +0800

    更新

---
+++ gen_down_web.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+IPA_BASE_URL="https://dev.shgsec.com:1782/builds/iOS/"
+list=$(ls | grep "sgzq-phone")
+for file in ${list[@]}
+do
+	file_path=`echo ${file%.*}`
+	mkdir -p ./versions/${file_path}
+	cp -a ./template_down/* ./versions/${file_path}
+    IPA_URL="${IPA_BASE_URL}${file}"
+    IPA_URL=${IPA_URL//\//\\\/}
+    sed -i "" "s/IOS_APP_URL/${IPA_URL}/g" ./versions/${file_path}/shgsec_iphone.plist
+    echo "${file}替换plist文件完成"
+done
